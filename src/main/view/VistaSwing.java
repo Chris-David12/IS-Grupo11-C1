a866ccc6483ff5aaca2a5a3d6c0b18686bce457f
Joseph-Alexn <167446989+Joseph-Alexn@users.noreply.github.com>
2025-07-14 19:33:14 -0400

    primera prueba

---
+++ src/main/view/VistaSwing.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class VistaSwing {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Mi Vista Java");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+
+        JLabel etiqueta = new JLabel("Nombre:");
+        JTextField campoTexto = new JTextField(20);
+        JButton boton = new JButton("Prueba");
+        JLabel prueba = new JLabel("");
+
+        boton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                prueba.setText("HOLA " + campoTexto.getText() + "!");
+            }
+        });
+
+        panel.add(etiqueta);
+        panel.add(campoTexto);
+        panel.add(boton);
+        panel.add(prueba);
+
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+}
